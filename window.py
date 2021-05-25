d4c021bfa9c4a59e092e2f8e6ee90cdd914fc5e8
Govind <legovindpillai@gmail.com>
2021-05-24 22:17:08 -0500

    added onto GUI

--- window.py
+++ window.py
@@ -1,5 +1,23 @@
-from tkinter import Tk
+from tkinter import Tk, Button
+
+window = Tk()
+
+# defining constants
+SCREEN_WIDTH = window.winfo_screenwidth()
+SCREEN_HEIGHT = window.winfo_screenheight()
+WINDOW_WIDTH = window.winfo_reqwidth()  # int(SCREEN_WIDTH * .8)
+WINDOW_HEIGHT = window.winfo_reqheight()  # int(SCREEN_HEIGHT * .8)
+
+# setting window size to be 80% the size of the screen
+window.eval('tk::PlaceWindow . center')
 
 
-print("Hello world")
-Tk().mainloop()
+nextBtn = Button(
+    window,
+    text="Next",
+    width=int(0.05 * WINDOW_WIDTH),
+    height=int(0.01 * WINDOW_HEIGHT)
+)
+
+nextBtn.pack()
+window.mainloop()
